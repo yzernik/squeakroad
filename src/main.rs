fa5ad8ece0c87786dbb45a8e6b925ed709678b78
Jonathan Zernik <yzernik@gmail.com>
2022-06-11 21:52:31 -0700

    Remove non sqlx database libs (#1)
    * Remove diesel
    
    * Remove rusqlite
    
    * Mount sqlx routes at base url

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_sync_db_pools;
-#[macro_use] extern crate diesel_migrations;
-#[macro_use] extern crate diesel;
+#[macro_use]
+extern crate rocket;
 
-#[cfg(test)] mod tests;
+#[cfg(test)]
+mod tests;
 
 mod sqlx;
-mod diesel_sqlite;
-mod rusqlite;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .attach(sqlx::stage())
-        .attach(rusqlite::stage())
-        .attach(diesel_sqlite::stage())
+    rocket::build().attach(sqlx::stage())
 }
