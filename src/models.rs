4e8f681c24d41622e592cdf12ae23d9215ba32d2
Jonathan Zernik <yzernik@gmail.com>
2022-06-25 13:12:14 -0700

    Comment out old delete image query

--- src/models.rs
+++ src/models.rs
@@ -390,14 +390,14 @@ impl ListingImage {
         Ok(update_result.rows_affected() as _)
     }
 
-    /// Returns the number of affected rows: 1.
-    pub async fn delete_with_id(id: i32, db: &mut Connection<Db>) -> Result<usize, sqlx::Error> {
-        let delete_result = sqlx::query!("DELETE FROM listingimages WHERE id = ?", id)
-            .execute(&mut **db)
-            .await?;
+    // /// Returns the number of affected rows: 1.
+    // pub async fn delete_with_id(id: i32, db: &mut Connection<Db>) -> Result<usize, sqlx::Error> {
+    //     let delete_result = sqlx::query!("DELETE FROM listingimages WHERE id = ?", id)
+    //         .execute(&mut **db)
+    //         .await?;
 
-        Ok(delete_result.rows_affected() as _)
-    }
+    //     Ok(delete_result.rows_affected() as _)
+    // }
 
     /// Returns the number of affected rows: 1.
     pub async fn delete_with_public_id(
