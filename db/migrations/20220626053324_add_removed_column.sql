6eb9a113293d05a14a0dce30f8190af6f2f16793
Jonathan Zernik <yzernik@gmail.com>
2022-06-25 22:47:07 -0700

    Add removed column to listing (#113)

---
+++ db/migrations/20220626053324_add_removed_column.sql
@@ -0,0 +1,2 @@
+ALTER TABLE listings
+ADD COLUMN removed BOOLEAN NOT NULL default 0;
--- src/new_listing.rs
+++ src/new_listing.rs
@@ -88,6 +88,7 @@ async fn create_listing(
         price_msat: listing_info.price_sat * 1000,
         submitted: false,
         approved: false,
+        removed: false,
         created_time_ms: now,
     };
 
