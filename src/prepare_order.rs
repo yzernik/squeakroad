d9916badcf7b2b17ec9a110507542b95f3ca76a8
Jonathan Zernik <yzernik@gmail.com>
2022-06-28 19:59:14 -0700

    Implement create order function

--- src/prepare_order.rs
+++ src/prepare_order.rs
@@ -1,14 +1,17 @@
 use crate::db::Db;
-use crate::models::{AdminSettings, Listing, ListingDisplay, ShippingOption};
+use crate::models::{AdminSettings, Listing, ListingDisplay, Order, ShippingOption};
 use rocket::fairing::AdHoc;
 use rocket::request::FlashMessage;
 use rocket::response::Flash;
 use rocket::response::Redirect;
+use rocket::serde::uuid::Uuid;
 use rocket::serde::Serialize;
 use rocket_auth::AdminUser;
 use rocket_auth::User;
 use rocket_db_pools::Connection;
 use rocket_dyn_templates::Template;
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
 
 #[derive(Debug, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -51,6 +54,63 @@ impl Context {
             admin_user,
             admin_settings: Some(admin_settings),
         })
+    }
+}
+
+async fn create_order(
+    quantity: u32,
+    listing_id: &str,
+    shipping_option_id: &str,
+    shipping_instructions: &str,
+    db: &mut Connection<Db>,
+    user: User,
+    _admin_user: Option<AdminUser>,
+) -> Result<(), String> {
+    let listing = Listing::single_by_public_id(db, listing_id)
+        .await
+        .map_err(|_| "failed to get listing")?;
+    let shipping_option = ShippingOption::single_by_public_id(db, shipping_option_id)
+        .await
+        .map_err(|_| "failed to get shipping option.")?;
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64;
+
+    let price_per_unit_with_shipping_sat: u64 = listing.price_sat + shipping_option.price_sat;
+    let amount_owed_sat: u64 = ((quantity as u64) * price_per_unit_with_shipping_sat).into();
+    let market_fee_sat: u64 = (amount_owed_sat * (listing.fee_rate_basis_points as u64)) / 10000;
+    let seller_credit_sat: u64 = amount_owed_sat - market_fee_sat;
+
+    if shipping_instructions.is_empty() {
+        Err("Shipping instructions cannot be empty.".to_string())
+    } else if shipping_instructions.len() > 1024 {
+        Err("Shipping instructions length is too long.".to_string())
+    } else if listing.user_id == user.id() {
+        Err("Listing belongs to same user as buyer.".to_string())
+    } else if shipping_option.listing_id != listing.id.unwrap() {
+        Err("Shipping option not associated with listing.".to_string())
+    } else {
+        let order = Order {
+            id: None,
+            public_id: Uuid::new_v4().to_string(),
+            quantity: quantity,
+            user_id: user.id(),
+            listing_id: listing.id.unwrap(),
+            shipping_option_id: shipping_option.id.unwrap(),
+            shipping_instructions: shipping_instructions.to_string(),
+            amount_owed_sat: amount_owed_sat,
+            seller_credit_sat: seller_credit_sat,
+            paid: false,
+            completed: false,
+            created_time_ms: now,
+        };
+
+        Order::insert(order, db)
+            .await
+            .map_err(|_| "failed to save order.")?;
+
+        Ok(())
     }
 }
 
