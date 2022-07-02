7e1528007af0d36ceba2595598e5d57df0db31eb
Jonathan Zernik <yzernik@gmail.com>
2022-07-02 13:54:22 -0700

    Add about page (#196)

---
+++ src/about.rs
@@ -0,0 +1,57 @@
+use crate::base::BaseContext;
+use crate::db::Db;
+use crate::models::AdminSettings;
+use rocket::fairing::AdHoc;
+use rocket::request::FlashMessage;
+use rocket::serde::Serialize;
+use rocket_auth::AdminUser;
+use rocket_auth::User;
+use rocket_db_pools::Connection;
+use rocket_dyn_templates::Template;
+
+#[derive(Debug, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Context {
+    base_context: BaseContext,
+    flash: Option<(String, String)>,
+    admin_settings: AdminSettings,
+}
+
+impl Context {
+    pub async fn raw(
+        mut db: Connection<Db>,
+        flash: Option<(String, String)>,
+        user: Option<User>,
+        admin_user: Option<AdminUser>,
+    ) -> Result<Context, String> {
+        let base_context = BaseContext::raw(&mut db, user.clone(), admin_user.clone())
+            .await
+            .map_err(|_| "failed to get base template.")?;
+        let admin_settings = AdminSettings::single(&mut db, AdminSettings::get_default())
+            .await
+            .map_err(|_| "failed to update market name.")?;
+        Ok(Context {
+            base_context,
+            flash,
+            admin_settings,
+        })
+    }
+}
+
+#[get("/")]
+async fn index(
+    flash: Option<FlashMessage<'_>>,
+    db: Connection<Db>,
+    user: Option<User>,
+    admin_user: Option<AdminUser>,
+) -> Template {
+    let flash = flash.map(FlashMessage::into_inner);
+    Template::render("about", Context::raw(db, flash, user, admin_user).await)
+}
+
+pub fn about_stage() -> AdHoc {
+    AdHoc::on_ignite("About Stage", |rocket| async {
+        rocket.mount("/about", routes![index])
+        // .mount("/listing", routes![new])
+    })
+}
