12e6f34ae46fb8c9c39568d2267bedf828081ed6
Jonathan Zernik <yzernik@gmail.com>
2022-07-04 17:15:43 -0700

    Read admin username and password from config

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct Config {
-    pub key: String,
+    pub admin_username: String,
+    pub admin_password: String,
     pub lnd_host: String,
     pub lnd_port: u32,
     pub lnd_tls_cert_path: String,
@@ -16,7 +17,8 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Config {
         Config {
-            key: "default".into(),
+            admin_username: "admin@gmail.com".into(),
+            admin_password: "pass".into(),
             lnd_host: "localhost".into(),
             lnd_port: 10009,
             lnd_tls_cert_path: "~/.lnd/tls.cert".into(),
