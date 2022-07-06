f28fde4749700918cdfea8e484132fdedf78c522
Jonathan Zernik <yzernik@gmail.com>
2022-07-05 17:36:25 -0700

    Add test for login as admin and update the market name

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,22 @@
 use crate::config::Config;
 use rocket::fairing::AdHoc;
-use rocket::http::Status;
+use rocket::http::uri::fmt::{Query, UriDisplay};
+use rocket::http::{ContentType, Status};
 use rocket::local::blocking::Client;
 use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize, UriDisplayQuery)]
+#[serde(crate = "rocket::serde")]
+struct LoginInfo {
+    email: String,
+    password: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize, UriDisplayQuery)]
+#[serde(crate = "rocket::serde")]
+struct UpdateMarketNameInfo {
+    market_name: String,
+}
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -11,7 +25,7 @@ struct Post {
     text: String,
 }
 
-fn test(base: &str, stage: AdHoc) {
+fn test_posts(base: &str, stage: AdHoc) {
     // Number of posts we're going to create/read/delete.
     const N: usize = 20;
 
@@ -69,9 +83,48 @@ fn test(base: &str, stage: AdHoc) {
     assert_eq!(response.status(), Status::NotFound);
 }
 
+fn test_admin_settings(_base: &str, stage: AdHoc, config: Config) {
+    // NOTE: If we had more than one test running concurently that dispatches
+    // DB-accessing requests, we'd need transactions or to serialize all tests.
+    let client = Client::tracked(rocket::build().attach(stage)).unwrap();
+
+    // Log in as admin user.
+    let admin_login_info = LoginInfo {
+        email: config.admin_username,
+        password: config.admin_password,
+    };
+    let login_response = client
+        .post("/login")
+        .header(ContentType::Form)
+        .body((&admin_login_info as &dyn UriDisplay<Query>).to_string())
+        .dispatch();
+    println!("login_response: {:?}", login_response);
+
+    // Update the market name.
+    let update_market_name_info = UpdateMarketNameInfo {
+        market_name: "test-market-name".to_string(),
+    };
+    let update_market_name_response = client
+        .post("/update_market_name/change")
+        .header(ContentType::Form)
+        .body((&update_market_name_info as &dyn UriDisplay<Query>).to_string())
+        .dispatch();
+    println!(
+        "update_market_name_response: {:?}",
+        update_market_name_response
+    );
+
+    // Get the index page and check the market name.
+    let index_page_response = client.get("/").dispatch();
+    let index_page_string = index_page_response.into_string().unwrap();
+    println!("{}", index_page_string);
+    assert!(index_page_string.contains("test-market-name"));
+}
+
 #[test]
 fn test_routes() {
     let config_figment = Config::get_config();
     let config: Config = config_figment.extract().unwrap();
-    test("/posts", crate::routes::stage(config))
+    test_posts("/posts", crate::routes::stage(config.clone()));
+    test_admin_settings("/", crate::routes::stage(config.clone()), config.clone());
 }
