3201de4cc97adb65a77a37b99dce082fbc9d211f
Jonathan Zernik <yzernik@gmail.com>
2022-07-05 20:28:11 -0700

    Got pagination working for my received orders page

--- src/my_received_orders.rs
+++ src/my_received_orders.rs
@@ -9,25 +9,30 @@ use rocket_auth::{AdminUser, User};
 use rocket_db_pools::Connection;
 use rocket_dyn_templates::Template;
 
+const PAGE_SIZE: u32 = 10;
+
 #[derive(Debug, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Context {
     base_context: BaseContext,
     flash: Option<(String, String)>,
     order_cards: Vec<OrderCard>,
+    page_num: u32,
 }
 
 impl Context {
     pub async fn raw(
         flash: Option<(String, String)>,
         mut db: Connection<Db>,
+        maybe_page_num: Option<u32>,
         user: User,
         admin_user: Option<AdminUser>,
     ) -> Result<Context, String> {
         let base_context = BaseContext::raw(&mut db, Some(user.clone()), admin_user.clone())
             .await
             .map_err(|_| "failed to get base template.")?;
-        let order_cards = OrderCard::all_received_for_user(&mut db, user.id)
+        let page_num = maybe_page_num.unwrap_or(1);
+        let order_cards = OrderCard::all_received_for_user(&mut db, user.id, PAGE_SIZE, page_num)
             .await
             .map_err(|_| "failed to get received orders.")?;
         println!("order_cards: {:?}", order_cards);
@@ -35,21 +40,23 @@ impl Context {
             base_context,
             flash,
             order_cards,
+            page_num,
         })
     }
 }
 
-#[get("/")]
+#[get("/?<page_num>")]
 async fn index(
     flash: Option<FlashMessage<'_>>,
     db: Connection<Db>,
+    page_num: Option<u32>,
     user: User,
     admin_user: Option<AdminUser>,
 ) -> Result<Template, NotFound<String>> {
     let flash = flash.map(FlashMessage::into_inner);
     Ok(Template::render(
         "myreceivedorders",
-        Context::raw(flash, db, user, admin_user).await,
+        Context::raw(flash, db, page_num, user, admin_user).await,
     ))
 }
 
