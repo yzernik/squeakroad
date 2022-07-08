066173ec8f3520606f4312ecb1b3dcd05eaec04d
Jonathan Zernik <yzernik@gmail.com>
2022-07-08 02:28:25 -0700

    Use qr_code library and bmp format for qr code image (#354)

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,6 @@
-use qrcode_generator::QrCodeEcc;
+use qr_code::QrCode;
 use rocket::serde::uuid::Uuid;
+use std::io::Cursor;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
 
@@ -15,7 +16,11 @@ pub fn current_time_millis() -> u64 {
 }
 
 pub fn generate_qr(payment_request: &str) -> Vec<u8> {
-    qrcode_generator::to_png_to_vec(payment_request, QrCodeEcc::Low, 128).unwrap()
+    let qr = QrCode::new(&payment_request.as_bytes()).unwrap();
+    let bmp = qr.to_bmp().add_white_border(4).unwrap().mul(4).unwrap();
+    let mut cursor = Cursor::new(vec![]);
+    bmp.write(&mut cursor).unwrap();
+    cursor.into_inner()
 }
 
 pub fn to_hex(bytes: &Vec<u8>) -> String {
