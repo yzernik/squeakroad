cb6ba80a3bebd6a3ac0b34bb2bfb261b475c1240
Jonathan Zernik <yzernik@gmail.com>
2022-07-09 01:09:50 -0700

    Remove old comment

--- src/lightning.rs
+++ src/lightning.rs
@@ -1,27 +1,12 @@
 use squeakroad_lnd_client::get_client;
 use squeakroad_lnd_client::LndClient;
 
-// pub async fn get_lnd_client(
-//     lnd_host: String,
-//     lnd_port: u32,
-//     lnd_tls_cert_path: String,
-//     lnd_macaroon_path: String,
-// ) -> Result<LightningClient<InterceptedService<Channel, MacaroonInterceptor>>, String> {
-//     let lnd_address = format!("http://{}:{}", lnd_host, lnd_port);
-//     let client = tonic_lnd::connect(lnd_address, lnd_tls_cert_path, lnd_macaroon_path)
-//         .await
-//         .map_err(|e| format!("failed to get LND client: {:?}", e))?;
-//     Ok(client)
-// }
-
 pub async fn get_lnd_client(
     lnd_host: String,
     lnd_port: u32,
     lnd_tls_cert_path: String,
     lnd_macaroon_path: String,
 ) -> Result<LndClient, String> {
-    // let lnd_address = format!("http://{}:{}", lnd_host, lnd_port);
-
     // TODO: don't use unwrap.
     let client = get_client(lnd_host, lnd_port, lnd_tls_cert_path, lnd_macaroon_path)
         .await
