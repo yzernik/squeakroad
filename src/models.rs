6e98e9f2d46806c44281765a459cb3bc845766be
Jonathan Zernik <yzernik@gmail.com>
2022-07-24 14:02:56 -0700

    Use sum query to get user account balance instead of manual iter sum.

--- src/models.rs
+++ src/models.rs
@@ -2533,12 +2533,7 @@ impl AccountInfo {
         db: &mut Connection<Db>,
         user_id: i32,
     ) -> Result<AccountInfo, sqlx::Error> {
-        let account_balance_changes =
-            AccountInfo::all_account_balance_changes_for_user(db, user_id, u32::MAX, 1).await?;
-        let account_balance_sat: i64 = account_balance_changes
-            .iter()
-            .map(|c| c.amount_change_sat)
-            .sum();
+        let account_balance_sat = AccountInfo::total_account_balance_for_user(db, user_id).await?;
         let unshipped_orders = OrderCard::all_pending_for_user(db, user_id, u32::MAX, 1).await?;
         let num_unshipped_orders = unshipped_orders.len();
         Ok(AccountInfo {
@@ -2612,51 +2607,50 @@ OFFSET ?
         Ok(account_balance_changes)
     }
 
-    // TODO: Use this when sqlx is fixed.
-    //     pub async fn total_account_balance_for_user(
-    //         db: &mut Connection<Db>,
-    //         user_id: i32,
-    //     ) -> Result<Vec<AccountBalanceChange>, sqlx::Error> {
-    //         // TODO: Order by event time in SQL query. When this is fixed: https://github.com/launchbadge/sqlx/issues/1350
-    //         let total_account_balance_result = sqlx::query!("
-    // SELECT SUM(amount_change_sat) as total_account_balance_sat FROM
-    // (select orders.seller_user_id as user_id, orders.seller_credit_sat as amount_change_sat, 'received_order' as event_type, orders.public_id as event_id, orders.created_time_ms as event_time_ms
-    // from
-    //  orders
-    // WHERE
-    //  orders.paid
-    // AND
-    //  orders.shipped
-    // AND
-    //  orders.seller_user_id = ?
-    // UNION ALL
-    // select orders.buyer_user_id as user_id, orders.amount_owed_sat as amount_change_sat, 'refunded_order' as event_type, orders.public_id as event_id, orders.created_time_ms as event_time_ms
-    // from
-    //  orders
-    // WHERE
-    //  orders.paid
-    // AND
-    //  not orders.shipped
-    // AND
-    //  orders.buyer_user_id = ?
-    // UNION ALL
-    // select withdrawals.user_id as user_id, (0 - withdrawals.amount_sat) as amount_change_sat, 'withdrawal' as event_type, withdrawals.public_id as event_id, withdrawals.created_time_ms as event_time_ms
-    // from
-    //  withdrawals
-    // WHERE
-    //  withdrawals.user_id = ?)
-    // LEFT JOIN
-    //  users
-    // ON
-    //  user_id = users.id
-    // ;",
-    //         user_id, user_id, user_id)
-    //             .fetch_one(&mut **db)
-    //             .map_ok(|r| r.total_account_balance_sat)
-    //             .await?;
+    pub async fn total_account_balance_for_user(
+        db: &mut Connection<Db>,
+        user_id: i32,
+    ) -> Result<i64, sqlx::Error> {
+        let account_balance_sat = sqlx::query("
+SELECT SUM(amount_change_sat) as total_account_balance_sat FROM
+(select orders.seller_user_id as user_id, orders.seller_credit_sat as amount_change_sat, 'received_order' as event_type, orders.public_id as event_id, orders.created_time_ms as event_time_ms
+from
+ orders
+WHERE
+ orders.paid
+AND
+ orders.shipped
+AND
+ orders.seller_user_id = ?
+UNION ALL
+select orders.buyer_user_id as user_id, orders.amount_owed_sat as amount_change_sat, 'refunded_order' as event_type, orders.public_id as event_id, orders.created_time_ms as event_time_ms
+from
+ orders
+WHERE
+ orders.paid
+AND
+ (orders.canceled_by_seller OR orders.canceled_by_buyer)
+AND
+ orders.buyer_user_id = ?
+UNION ALL
+select withdrawals.user_id as user_id, (0 - withdrawals.amount_sat) as amount_change_sat, 'withdrawal' as event_type, withdrawals.public_id as event_id, withdrawals.created_time_ms as event_time_ms
+from
+ withdrawals
+WHERE
+ withdrawals.user_id = ?)
+;")
+            .bind(user_id)
+            .bind(user_id)
+            .bind(user_id)
+            .fetch_one(&mut **db)
+            .map_ok(|r|  {
+                let balance_sat_i64: i64 = r.try_get("total_account_balance_sat").unwrap();
+                balance_sat_i64
+            })
+            .await?;
 
-    //         Ok(total_account_balance_result)
-    //     }
+        Ok(account_balance_sat)
+    }
 
     pub async fn all_account_balance_changes(
         db: &mut Connection<Db>,
@@ -2811,7 +2805,6 @@ from
  withdrawals
 WHERE
  withdrawals.user_id = ?)
-ORDER BY event_time_ms DESC
 ;")
             .bind(user_id)
             .bind(user_id)
