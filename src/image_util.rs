33dbda7ab8b59417e7d07ba404866221681d240b
Jonathan Zernik <yzernik@gmail.com>
2022-07-29 14:45:59 -0700

    Got strip metadata and delete temp file both working

--- src/image_util.rs
+++ src/image_util.rs
@@ -6,30 +6,33 @@ use std::io::Read;
 use std::path::Path;
 
 pub fn get_stripped_image_bytes(image_bytes: &Vec<u8>) -> Result<Vec<u8>, String> {
-    // Step 1: Save the image bytes into a file.
-    let path_string = format!("/tmp/image_file_path_{}.data", util::create_uuid());
+    let path_string = format!("/tmp/image_file_{}.data", util::create_uuid());
     let path: &Path = Path::new(&path_string);
     fs::write(path, image_bytes).map_err(|_| "failed to write image to file.")?;
 
-    // Step 2: Load the file into rexiv2 struct.
-    let metadata =
-        Metadata::new_from_path(path).map_err(|_| "failed to load metadata from image buffer.")?;
+    match get_stripped_image_from_path(path) {
+        Ok(data) => {
+            fs::remove_file(path).ok();
+            Ok(data)
+        }
+        Err(e) => {
+            fs::remove_file(path).ok();
+            Err(e)
+        }
+    }
+}
 
-    // Step 3: Strip the metadata from the struct.
+pub fn get_stripped_image_from_path(file_path: &Path) -> Result<Vec<u8>, String> {
+    let metadata = Metadata::new_from_path(file_path)
+        .map_err(|_| "failed to load metadata from image buffer.")?;
     metadata.clear();
-
-    // Step 4: Save the rexiv2 struct back to the file.
     metadata
-        .save_to_file(path)
+        .save_to_file(file_path)
         .map_err(|_| "failed to save cleared metadata back to file.")?;
-
-    // Step 5: Read the file back into memory.
-    let mut f = File::open(path).map_err(|_| "failed to open the cleared image file.")?;
+    let mut f = File::open(file_path).map_err(|_| "failed to open the cleared image file.")?;
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)
         .map_err(|_| "failed to read cleared image file to bytes.")?;
-
-    // Step 6: Delete all intermediate files.
 
     Ok(buffer)
 }
